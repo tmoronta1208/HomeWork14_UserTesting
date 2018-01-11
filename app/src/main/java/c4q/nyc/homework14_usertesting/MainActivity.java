8cae6818ab544cbfc3d3ec1c1a4cde9f4d0d08ff
Ackochanski <AnneKochanski@ac.c4q.nyc>
2018-01-11 15:03:56 -0500

    xml and recycler view skeleton

--- app/src/main/java/c4q/nyc/homework14_usertesting/MainActivity.java
+++ app/src/main/java/c4q/nyc/homework14_usertesting/MainActivity.java
@@ -2,6 +2,10 @@ package c4q.nyc.homework14_usertesting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import c4q.nyc.homework14_usertesting.controller.NewsFeedAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView newsRecyclerView = findViewById(R.id.news_recyclerview);
+        NewsFeedAdapter newsAdapter = new NewsFeedAdapter();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),3);
+        newsRecyclerView.setAdapter(newsAdapter);
+        newsRecyclerView.setLayoutManager(gridLayoutManager);
     }
 }
---
+++ app/src/main/java/c4q/nyc/homework14_usertesting/view/NewsFeedViewHolder.java
@@ -0,0 +1,16 @@
+package c4q.nyc.homework14_usertesting.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by c4q on 1/11/18.
+ */
+
+public class NewsFeedViewHolder extends RecyclerView.ViewHolder {
+
+
+    public NewsFeedViewHolder(View itemView) {
+        super(itemView);
+    }
+}
