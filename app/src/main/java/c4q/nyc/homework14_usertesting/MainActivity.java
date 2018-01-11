26333e7de234df5ee0ab95faac88260355182185
Ackochanski <AnneKochanski@ac.c4q.nyc>
2018-01-11 14:24:40 -0500

    initial commit

---
+++ app/src/main/java/c4q/nyc/homework14_usertesting/MainActivity.java
@@ -0,0 +1,13 @@
+package c4q.nyc.homework14_usertesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
